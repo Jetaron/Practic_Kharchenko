ec215482c9eaeda8ca53e8338c0d8a89be15689c
Jetaron <150559078+Jetaron@users.noreply.github.com>
2025-06-16 08:35:20 +0300

    Add initial components including Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Переконайся, що тут правильний шлях і розширення
+import './index.css';     // Або твої глобальні стилі
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
